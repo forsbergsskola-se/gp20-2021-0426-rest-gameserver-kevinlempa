909b1fc93fb3c65fe2e3b9cec6a1148b3f4b2e1f
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-04-28 10:37:03 +0200

    get the html doc from acme.com

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Sockets;
+using System.Text;
 
-namespace TinyBrowser
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace TinyBrowser {
+    class Program {
+        static void Main(string[] args) {
+            TcpClient client = new TcpClient();
+            byte[] bytes = new byte[11000];
+            string html;
+            while (true) {
+                client.Connect("www.acme.com", 80);
+                if (client.Connected) {
+                    var stream = client.GetStream();
+                    var buffer = Encoding.ASCII.GetBytes("GET / HTTP/1.1"+Environment.NewLine +"Host: acme.com"+Environment.NewLine+Environment.NewLine);
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Write(buffer, 0, buffer.Length);
+                    stream.Read(bytes, 0, bytes.Length);
+                    html = Encoding.ASCII.GetString(bytes);
+                    Console.WriteLine(html);
+                    stream.Flush();
+                    stream.Close();
+                    client.Close();
+                }
+                break;
+            }
+            
+            
         }
     }
 }
