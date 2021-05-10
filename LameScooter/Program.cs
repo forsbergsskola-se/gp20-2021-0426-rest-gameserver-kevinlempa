f52d2610cd515317bb67fc50a75281ecf465c168
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-05-10 23:55:26 +0200

    improved argument exception message.

--- LameScooter/Program.cs
+++ LameScooter/Program.cs
@@ -1,24 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace LameScooter {
     class Program {
         static async Task Main(string[] args) {
             try {
-                ILameScooterRental rental = new OfflineLameScooterRental();
-                ILameScooterRental rental2 = new DeprecatedLameScooterRental();
-                var s = await rental2.GetScooterCountInStation(args[0]);
-                var num = await rental.GetScooterCountInStation(args[0]);
-                Console.WriteLine("Deprecated : Number of Scooters Available at this Station: " + s);
-                Console.WriteLine("Number of Scooters Available at this Station: " + num);
+                ILameScooterRental rental = args[1] switch {
+                    "offline" => new OfflineLameScooterRental(),
+                    "deprecated" => new DeprecatedLameScooterRental(),
+                    "realtime" => new RealTimeLameScooterRental(),
+                    _ => throw new ArgumentException($"{args[1]} is not a valid argument")
+                };
+                var availableBikes = await rental.GetScooterCountInStation(args[0]);
+                Console.WriteLine($"Number of Scooters Available at {args[0]} : {availableBikes}");
             }
             catch (Exception e) {
-                throw new ArgumentException("Invalid argument \r\n" +e);
+                Console.WriteLine($"Invalid Argument: \r\nHere are some valid Argument examples:\r\n" +
+                                  $"#1: dotnet run \"Meilahden sairaala\" offline \r\n" +
+                                  $"#2: dotnet run Vantaanpuisto realtime \r\n" +
+                                  $"#3: dotnet run Huopalahdentie deprecated \r\n" +
+                                  $" \r\n{e.Message}");
+                throw;
             }
-            
         }
     }
 }
